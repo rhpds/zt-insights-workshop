ae55e063d388896f6ac811ae4885294251b3f6f8
Matthew Yee <myee@redhat.com>
2025-09-12 14:53:53 -0700

    Add README.md for Insights Advisor Rules and update setup-rhel.sh to include permissions for sshd_config and shadow files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Insights Advisor Rules
+
+```
+chmod 777 /etc/ssh/sshd_config
+chmod +0777 /etc/shadow
+```
--- runtime-automation/02-insights-login/setup-rhel.sh
+++ runtime-automation/02-insights-login/setup-rhel.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Starting module called 02-insights-login" >> /tmp/progress.log
 
-sed -i 's/ClientAliveInterval.*900//g' /etc/ssh/sshd_config
-sed -i '/\[rhel-10-for-x86_64-baseos-rpms\]/,/^ *\[/ s/gpgcheck = 1/gpgcheck = 0/' /etc/yum.repos.d/redhat.repo
-sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# sed -i 's/ClientAliveInterval.*900//g' /etc/ssh/sshd_config
+# sed -i '/\[rhel-10-for-x86_64-baseos-rpms\]/,/^ *\[/ s/gpgcheck = 1/gpgcheck = 0/' /etc/yum.repos.d/redhat.repo
+# sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+chmod 777 /etc/ssh/sshd_config
+chmod +0777 /etc/shadow
 insights-client
