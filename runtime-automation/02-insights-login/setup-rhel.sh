3ffb265a5b52a012017c18e47a2bb0b2ac18e2c9
Matthew Yee <myee@redhat.com>
2025-09-12 13:55:40 -0700

    Add images for rhc connect output and rhc worker playbook installation to documentation

--- runtime-automation/02-insights-login/setup-rhel.sh
+++ runtime-automation/02-insights-login/setup-rhel.sh
@@ -5,4 +5,3 @@ sed -i 's/ClientAliveInterval.*900//g' /etc/ssh/sshd_config
 sed -i '/\[rhel-10-for-x86_64-baseos-rpms\]/,/^ *\[/ s/gpgcheck = 1/gpgcheck = 0/' /etc/yum.repos.d/redhat.repo
 hostnamectl set-hostname rhel-$(uuidgen | cut -c 32-)
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-exec $SHELL
