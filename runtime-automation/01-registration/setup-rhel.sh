e8e1d9895609c95c4194d3412173e2ab8dc66f23
Matthew Yee <myee@redhat.com>
2025-09-12 13:24:55 -0700

    Update site.yml, ui-config.yml, firewall.yaml, instances.yaml, and registration scripts for RHEL 10 compatibility and Insights integration

--- runtime-automation/01-registration/setup-rhel.sh
+++ runtime-automation/01-registration/setup-rhel.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 echo "Starting module called 01-registration" >> /tmp/progress.log
+
+#!/bin/sh
+echo "Starting module called 01-timezone" >> /tmp/progress.log
+
+KATELLO=$(rpm -qa | grep katello-ca)
+
+rpm -e $KATELLO
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
