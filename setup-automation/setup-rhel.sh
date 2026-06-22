2d3f9bb6f54e87076f4bfa437c55f9b2ca577262
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:19 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 hostnamectl set-hostname rhel-$(uuidgen | cut -c 32-)
